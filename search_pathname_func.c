2f41e478811f3f036e683becb2c7cfea4532020b
Khloud Ayman <khloudayman780@gmail.com>
2023-12-13 06:07:54 -0800

    search

---
+++ search_pathname_func.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * search_pathname_func - Un archivo de búsqueda entre la ruta.
+ * @command: cmd print.
+ * Return: cmd, print pathname.
+ */
+
+char *search_pathname_func(char *command)
+{
+	char *pathname = _getenv("PATH"), *pathname_cpy;
+	char **pathname_split;
+	char *pathname_concat = NULL;
+	int g = 0, pathname_len = 0;
+	struct stat info;
+
+	if (stat(command, &info) == 0)
+		return (command);
+
+	pathname_cpy = malloc(_strlen(pathname) + 1);
+
+	pathname_cpy = _strcpy(pathname_cpy, pathname);
+	pathname_split = _split(pathname_cpy, ":");
+
+	while (pathname_split[g])
+	{
+		pathname_len = _strlen(pathname_split[g]);
+
+		if (pathname_split[g][pathname_len - 1] != '/')
+			pathname_concat = _strcat(pathname_split[g], "/");
+
+		pathname_concat = _strcat(pathname_split[g], command);
+
+		if (stat(pathname_concat, &info) == 0)
+			break;
+
+		i++;
+	}
+
+	free(pathname_cpy);
+
+	if (!pathname_split[g])
+	{
+		free(pathname_split);
+		return (NULL);
+	}
+
+	free(pathname_split);
+	return (pathname_concat);
+}
